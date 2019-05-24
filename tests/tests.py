6d5cfb1b2806daca1974ce3de697385571cfe5d3
Morgan Williams <wmorgan85@users.noreply.github.com>
2019-05-24 15:20:06 +0100

    fixed fraction tests

--- tests/tests.py
+++ tests/tests.py
@@ -10,7 +10,7 @@ class TestSum(unittest.TestCase):
         self.assertEqual(sum([1, 2, 3]), 6, "should be 6")
 
     def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "should be 6")
+        self.assertEqual(sum((1, 2, 3)), 6, "should be 6")
 
     def test_list_int(self):
         """
@@ -32,7 +32,7 @@ class TestSum(unittest.TestCase):
         """
         Test that it can sum a list of fractions
         """
-        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 4)]
         result = sum(data)
         self.assertEqual(result, 1)
 
